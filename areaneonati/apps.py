4dcd60ea8c1bef6dfc1e5b52ad2a220a521162a1
WRK0151\daniele.asteggiante <daniele.asteggiante@netservice.eu>
2024-10-10 12:23:09 +0200

    Initial commit

---
+++ areaneonati/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AreaneonatiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'areaneonati'
---
+++ classifiche/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ClassificheConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'classifiche'
